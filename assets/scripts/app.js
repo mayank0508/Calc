e65c8fd3dc4eb1281e6c7886263c39c116bd9e9c
mayank0508 <thakurmayank0508@gmail.com>
2021-01-14 20:46:52 +0530

    new

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,16 @@
 const defaultResult = 0;
 let currrentResult = defaultResult;
 
+function add(num1, num2){
+    const result = num1+num2;
+    alert('The result is ' + result);
+}
+
+add(3, 5);
+
  currrentResult = (currrentResult+10) *3 /2 -4;
 
  let calculatedescription = `(${defaultResult}+10) * 3 / 2 - 4`;
-
+ 
  outputResult(currrentResult, calculatedescription);
+
