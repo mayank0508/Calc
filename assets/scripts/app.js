37be2a4915be071e8eeb632a05ae617bc93bb0e9
mayank0508 <thakurmayank0508@gmail.com>
2021-01-14 18:56:31 +0530

    new

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,6 +4,5 @@ let currrentResult = defaultResult;
  currrentResult = (currrentResult+10) *3 /2 -4;
 
  let calculatedescription = `(${defaultResult}+10) * 3 / 2 - 4`;
- let errorMessage = 'An Errror \n' +
-  "Occured";
- outputResult(currrentResult, errorMessage);
+
+ outputResult(currrentResult, calculatedescription);
