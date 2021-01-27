964a5f42519b94f3b6988e714ebe64665fd824f7
mayank0508 <thakurmayank0508@gmail.com>
2021-01-27 21:03:41 +0530

    new

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,9 +2,9 @@ const defaultResult = 0;
 let currrentResult = defaultResult;
 
 function add(){
-    currrentResult = currrentResult + parseInt(userInput.value); //Here you can not use ParseInt, you can also do 
-                                                                 // something like this :-
-                                                                 // currrentResult = currrentResult + +(userInput.value)
+    currrentResult = currrentResult + parseInt(userInput.value); //Here you can not use ParseInt, you can also do
+                 //currrentResult.toString                                            // something like this :-
+                 // this is used to convert a number to a string                                                // currrentResult = currrentResult + +(userInput.value)
     outputResult(currrentResult, '');
 }
 
