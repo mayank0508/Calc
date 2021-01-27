edab84c1fa112f9ec5965c565633c0902883b036
mayank0508 <thakurmayank0508@gmail.com>
2021-01-27 20:59:48 +0530

    new

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,7 +2,9 @@ const defaultResult = 0;
 let currrentResult = defaultResult;
 
 function add(){
-    currrentResult = currrentResult + parseInt(userInput.value);
+    currrentResult = currrentResult + parseInt(userInput.value); //Here you can not use ParseInt, you can also do 
+                                                                 // something like this :-
+                                                                 // currrentResult = currrentResult + +(userInput.value)
     outputResult(currrentResult, '');
 }
 
