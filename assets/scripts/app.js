0b23bfa08b9a02e17ef5d03d9151c4a9bbc14b7f
mayank0508 <thakurmayank0508@gmail.com>
2021-01-28 19:16:39 +0530

    new

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,14 @@
 const defaultResult = 0;
 let currrentResult = defaultResult;
 
+function getUserInput() {
+    return enteredNumber;
+}
+
 function add(){
-    const calcDescription = `${currrentResult}+ ${userInput.value}`
-    currrentResult = currrentResult + parseInt(userInput.value); //Here you can not use ParseInt, you can also do
+    const enteredNumber = parseInt(userInput.value);
+    const calcDescription = `${currrentResult} + ${enteredNumber}`
+    currrentResult = currrentResult + enteredNumber; //Here you can not use ParseInt, you can also do
                  //currrentResult.toString                                            // something like this :-
                  // this is used to convert a number to a string                     // currrentResult = currrentResult + +(userInput.value)
     outputResult(currrentResult, calcDescription);
