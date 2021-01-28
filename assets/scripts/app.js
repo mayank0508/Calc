85f076e32f4886566247c592b91abbd99cd4921a
mayank0508 <thakurmayank0508@gmail.com>
2021-01-28 19:09:37 +0530

    new

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,10 +2,11 @@ const defaultResult = 0;
 let currrentResult = defaultResult;
 
 function add(){
+    const calcDescription = `${currrentResult}+ ${userInput.value}`
     currrentResult = currrentResult + parseInt(userInput.value); //Here you can not use ParseInt, you can also do
                  //currrentResult.toString                                            // something like this :-
                  // this is used to convert a number to a string                     // currrentResult = currrentResult + +(userInput.value)
-    outputResult(currrentResult, '');
+    outputResult(currrentResult, calcDescription);
 }
 
 addBtn.addEventListener('click', add);
