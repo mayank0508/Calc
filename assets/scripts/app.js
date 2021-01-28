c5aa465ae5d8805dae2738708af5c7f558b77dec
mayank0508 <thakurmayank0508@gmail.com>
2021-01-28 19:04:54 +0530

    new

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,7 +4,7 @@ let currrentResult = defaultResult;
 function add(){
     currrentResult = currrentResult + parseInt(userInput.value); //Here you can not use ParseInt, you can also do
                  //currrentResult.toString                                            // something like this :-
-                 // this is used to convert a number to a string                                                // currrentResult = currrentResult + +(userInput.value)
+                 // this is used to convert a number to a string                     // currrentResult = currrentResult + +(userInput.value)
     outputResult(currrentResult, '');
 }
 
