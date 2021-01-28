e26114fcde54f743aa0be0bef5b457778a3aeed2
mayank0508 <thakurmayank0508@gmail.com>
2021-01-28 19:51:41 +0530

    new

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,17 +5,45 @@ function getUserInput() {
     return enteredNumber;
 }
 
-function add(){
-    const enteredNumber = parseInt(userInput.value);
-    const calcDescription = `${currrentResult} + ${enteredNumber}`
-    currrentResult = currrentResult + enteredNumber; //Here you can not use ParseInt, you can also do
-                 //currrentResult.toString                                            // something like this :-
-                 // this is used to convert a number to a string                     // currrentResult = currrentResult + +(userInput.value)
+function operationResult(operator, startResult, endResult){
+    const calcDescription = `${startResult} ${operator} ${endResult}`
     outputResult(currrentResult, calcDescription);
 }
 
-addBtn.addEventListener('click', add);
+function add(){
+    const enteredNumber = getUserInput;
+    const initialResult = currrentResult;
+    currrentResult = currrentResult + enteredNumber;; //Here you can not use ParseInt, you can also do
+                 //currrentResult.toString                                            // something like this :-
+                 // this is used to convert a number to a string                     // currrentResult = currrentResult + +(userInput.value)
+    operationResult('+', initialResult, enteredNumber)
+}
 
+function subt(){
+    const enteredNumber = getUserInput;
+    const initialResult = currrentResult;
+    currrentResult = currrentResult - enteredNumber;
+    operationResult('-', initialResult, enteredNumber)
+}
+
+function multi(){
+    const enteredNumber = getUserInput;
+    const initialResult = currrentResult;
+    currrentResult = currrentResult - enteredNumber;
+    operationResult('-', initialResult, enteredNumber)
+}
+
+function divide(){
+    const enteredNumber = getUserInput;
+    const initialResult = currrentResult;
+    currrentResult = currrentResult - enteredNumber;
+    operationResult('-', initialResult, enteredNumber)
+}
+
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subt);
+multiplyBtn.addEventListener('click', multi);
+divideBtn.addEventListener('click', divide);
 
  
 
